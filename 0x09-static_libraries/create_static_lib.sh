b1ac212f21770f7409ef196ab91175b0a8346e43
Gabosk1 <agabsokwori@gmail.com>
2023-07-09 16:15:14 +0000

    fixed the error

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -c *.c
-ar -rc liball.a *.o
-ranlib liball.a
+
+# Find all the .c files in the current directory
+c_files=$(find . -name "*.c")
+
+# Compile each .c file into an object file
+for file in $c_files; do
+    gcc -c "$file" -o "${file%.c}.o"
+done
+
+# Create the static library
+ar rcs liball.a *.o
+
+# Clean up the object files
+rm -f *.o
+
+echo "Static library liball.a created successfully!"
