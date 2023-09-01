9b5a359fa1660cbd5e01cb416e7f8e36999f4fd3
Gabosk1 <agabsokwori@gmail.com>
2023-09-01 21:03:04 +0000

    Initial commmit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,18 +5,20 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-        int n;
-        unsigned int d_val = 0;
+	int n;
+	unsigned int d_val = 0;
 
-        if (!b)
-                return (0);
-
-        for (n = 0; b[n]; n++)
-        {
-                if (b[n] < '0' || b[n] > '1')
-                        return (0);
-                d_val = 2 * d_val + (b[n] - '0');
-        }
-
-        return (d_val);
+	if (!b)
+	{
+		return (0);
+	}
+	for (n = 0; b[n]; n++)
+	{
+		if (b[n] < '0' || b[n] > '1')
+		{
+			return (0);
+		}
+		d_val = 2 * d_val + (b[n] - '0');
+	}
+	return (d_val);
 }
